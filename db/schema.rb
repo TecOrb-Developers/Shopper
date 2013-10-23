a38567851c42ce32e61b25281b43eb331f7b1c37
Adam Cooke <adam@atechmedia.com>
2013-10-23 18:37:02 +0100

    updates to shoppe

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131023122504) do
+ActiveRecord::Schema.define(version: 20131007121147) do
 
   create_table "nifty_attachments", force: true do |t|
     t.integer  "parent_id"
