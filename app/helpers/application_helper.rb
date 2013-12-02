0618978f3cf400180c1c7fc66cfd6ac74e8e4b5d
Adam Cooke <adam@atechmedia.com>
2013-12-02 10:19:31 +0000

    somehow forgot to add the methods for the delivery cutoff calcuation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,5 @@
 module ApplicationHelper
   
-  def time_until_next_delivery_cutoff
-    '4 hours'
-  end
-  
   def active_nav_item(item)
     @active_nav_item = item
   end
@@ -19,6 +15,34 @@ module ApplicationHelper
     return '' if text.blank?
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true)
     markdown.render(text).html_safe
+  end
+  
+  def next_delivery_cutoff
+    fourpm = Time.now.change(:hour => 16, :minute => 0)
+    fourpm = (fourpm + 1.day).change(:hour => 16, :minute => 0) if fourpm < Time.now
+    fourpm = (fourpm + 1.day).change(:hour => 16, :minute => 0) if fourpm.sunday?
+    fourpm = (fourpm + 2.day).change(:hour => 16, :minute => 0) if fourpm.saturday?
+    fourpm
+  end
+  
+  def time_until_next_delivery_cut_off
+    minutes = (next_delivery_cutoff - Time.now) / 60
+    whole_hours = (minutes / 60).floor
+    remaining_minutes = (minutes - (whole_hours * 60)).to_i
+    Array.new.tap do |a|
+      a << pluralize(whole_hours, 'hour') if whole_hours > 0
+      a << pluralize(remaining_minutes, 'minute') if remaining_minutes > 0
+    end.to_sentence
+  end
+  
+  def next_delivery_day
+    date = next_delivery_cutoff.to_date + 1.day
+    case date
+    when Date.tomorrow
+      "tomorrow"
+    else
+      "on " + date.strftime("%A")
+    end
   end
   
 end
